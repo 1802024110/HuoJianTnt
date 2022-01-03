fb818d5a7ed97cc4ab70991b3776169277bbf2bb
NoDream <1802024110@qq.com>
2022-01-03 10:51:16 +0800

    1

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-import os
+import sys
 import json
 import login 
 import getWebStatu
 baseUrl = getWebStatu.getWebUrl()
 checkUrl = baseUrl + 'user/checkin'
-username, password = str(os.environ['username']), str(os.environ['password'])
-corpid=str(os.environ['corpid'])
+username = sys.argv[1]
+password = sys.argv[2]
+corpid = sys.argv[3]
 # 企业ID
-corpsecret=str(os.environ['corpsecret'])
+corpsecret = sys.argv[4]
 # 应用的凭证密钥
-agentid=str(os.environ['agentid'])
+agentid = sys.argv[5]
 # 应用的ID
 """ corpid='wwb330a036235c91ea'
 corpsecret='bpKk0puHo__K2WM2C4SDxZFRDOfxgFJnvW_vQy6HmhA'
 agentid='1000002'
 username, password = '1802024110@qq.com', '2002zengyuan' """
 if __name__ == "__main__":
+  print("用户名：" + username)
+  print("密码：" + password)
+  print("企业ID：" + corpid)
+  print("应用的凭证密钥：" + corpsecret)
+  print("应用的ID：" + agentid)
   loginCode =  login.login(username, password)
   if loginCode != 1: 
     print('登录失败')
@@ -23,9 +29,10 @@ if __name__ == "__main__":
   # 请求签到
   checkin = session.post(checkUrl).text
   checkin = json.loads(checkin)
-  # 签到状态
-  if checkin['ret'] == 1:
-    session.get(f'https://api.htm.fun/api/Wechat/text/?corpid={corpid}&corpsecret={corpsecret}&agentid={agentid}&text=火箭TNT签到成功')
-  else:
-    msg = checkin['msg']
-    session.get(f'https://api.htm.fun/api/Wechat/text/?corpid={corpid}&corpsecret={corpsecret}&agentid={agentid}&text=火箭TNT签到失败，原因：{msg}')
+  if corpid != '':
+    # 签到状态
+    if checkin['ret'] == 1:
+      session.get(f'https://api.htm.fun/api/Wechat/text/?corpid={corpid}&corpsecret={corpsecret}&agentid={agentid}&text=火箭TNT签到成功')
+    else:
+      msg = checkin['msg']
+      session.get(f'https://api.htm.fun/api/Wechat/text/?corpid={corpid}&corpsecret={corpsecret}&agentid={agentid}&text=火箭TNT签到失败，原因：{msg}')
