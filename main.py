10db5cab015d3d5a2fc6b62afe8992cf2e0528db
NoDream <1802024110@qq.com>
2022-08-22 20:26:59 +0800

    gx

--- main.py
+++ main.py
@@ -20,13 +20,14 @@ if __name__ == "__main__":
   # 请求签到
   checkin = session.post(checkUrl).text
   checkin = json.loads(checkin)
-  print("username: ",username+"似懂非懂","CORPID: ",CORPID+"似懂非懂","获取的参数")
   # 签到状态
   if checkin['ret'] == 1:
     AGENTID1 = os.environ['AGENTID1'] 
+    print(AGENTID1, CORPID, CORPSECRET)
     wecom = WeCom_nodream.WeCom(AGENTID1, CORPID, CORPSECRET)
     wecom.sendCardMsg("火箭TNT签到成功",str(checkin['msg']),'https://github.com/1802024110/HuoJianTnt','火箭TNT')
   else:
-    AGENTID1 = os.environ['AGENTID2'] 
-    wecom = WeCom_nodream.WeCom(AGENTID1, CORPID, CORPSECRET)
+    AGENTID2 = os.environ['AGENTID2'] 
+    print(AGENTID2, CORPID, CORPSECRET)
+    wecom = WeCom_nodream.WeCom(AGENTID2, CORPID, CORPSECRET)
     wecom.sendCardMsg("火箭TNT签到失败",str(checkin['msg']),'https://github.com/1802024110/HuoJianTnt','火箭TNT')
