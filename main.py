96d894934a796b7fe64ae2de3f952cb8d341245b
NoDream <1802024110@qq.com>
2022-08-22 20:30:16 +0800

    更新

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ if __name__ == "__main__":
   # 签到状态
   if checkin['ret'] == 1:
     AGENTID1 = os.environ['AGENTID1'] 
-    print(AGENTID1, CORPID, CORPSECRET)
+    print(AGENTID1, CORPID, CORPSECRET,"1")
     wecom = WeCom_nodream.WeCom(AGENTID1, CORPID, CORPSECRET)
     wecom.sendCardMsg("火箭TNT签到成功",str(checkin['msg']),'https://github.com/1802024110/HuoJianTnt','火箭TNT')
   else:
     AGENTID2 = os.environ['AGENTID2'] 
-    print(AGENTID2, CORPID, CORPSECRET)
+    print(AGENTID2, CORPID, CORPSECRET,"-1")
     wecom = WeCom_nodream.WeCom(AGENTID2, CORPID, CORPSECRET)
     wecom.sendCardMsg("火箭TNT签到失败",str(checkin['msg']),'https://github.com/1802024110/HuoJianTnt','火箭TNT')
