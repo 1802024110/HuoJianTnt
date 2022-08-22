df4cb69911ea7ade1b712db886b9c21057d60478
NoDream <1802024110@qq.com>
2022-08-22 19:54:24 +0800

    gx

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
 import sys
 import json
 import login 
 import getWebStatu
+import WeCom_nodream
 baseUrl = getWebStatu.getWebUrl()
 checkUrl = baseUrl + 'user/checkin'
 username = sys.argv[1]
@@ -13,11 +15,6 @@ corpsecret = sys.argv[4]
 agentid = sys.argv[5]
 # 应用的ID
 if __name__ == "__main__":
-  print("用户名：" + username)
-  print("密码：" + password)
-  print("企业ID：" + corpid)
-  print("应用的凭证密钥：" + corpsecret)
-  print("应用的ID：" + agentid)
   loginCode =  login.login(username, password)
   if loginCode != 1: 
     print('登录失败')
@@ -25,11 +22,16 @@ if __name__ == "__main__":
   # 请求签到
   checkin = session.post(checkUrl).text
   checkin = json.loads(checkin)
+  # 初始化企业微信参数
+  CORPID = os.environ['CORPID'] 
+  CORPSECRET = os.environ['CORPSECRET']
   if corpid != '':
     # 签到状态
     if checkin['ret'] == 1:
-      session.get(f'https://api.htm.fun/api/Wechat/text/?corpid={corpid}&corpsecret={corpsecret}&agentid={agentid}&text=火箭TNT签到成功')
+      AGENTID1 = os.environ['AGENTID1'] 
+      wecom = WeCom_nodream.WeCom(AGENTID1, CORPID, CORPSECRET)
+      wecom.sendCardMsg("火箭TNT签到成功",str(checkin['msg']),'https://github.com/1802024110/HuoJianTnt','火箭TNT')
     else:
-      msg = checkin['msg']
-      url = f"https://api.007666.xyz//wecom/send/card?corpid=wwb330a036235c91ea&corpsecret=k6qvy2LJTzkXZtS2kED8hnv53-a780Q6cFhWcPnVpnQ&agentid=1000003&content={msg}&title=火箭TNT签到失败&url=https://github.com/1802024110/HuoJianTnt"
-      response = requests.request("GET", url)
+      AGENTID1 = os.environ['AGENTID2'] 
+      wecom = WeCom_nodream.WeCom(AGENTID1, CORPID, CORPSECRET)
+      wecom.sendCardMsg("火箭TNT签到失败",str(checkin['msg']),'https://github.com/1802024110/HuoJianTnt','火箭TNT')
